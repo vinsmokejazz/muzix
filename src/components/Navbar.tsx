7e4153c2d2abdee12e3590a7e17ee7c090d85aa9
vinsmokejazz <vinsmokejazz@gmail.com>
2025-05-13 14:45:23 +0530

    navbar upd

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,9 +5,29 @@ import { cn } from "@/utils/cn";
 
 import Link from "next/link";
 
-export default function  Navbar(){
+export default function Navbar({ className }: { className?: string }) {
+  const [active, setActive] = useState<string | null>(null);
 
-  return <div>
-    Navbar
+  return <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}>
+
+    <Menu setActive={setActive}>
+      <Link href={"/"}>
+        <MenuItem setActive={setActive} active={active} item="Home">
+        </MenuItem>
+      </Link>
+
+      <MenuItem setActive={setActive} active={active} item="Courses">
+        <div className="flex flex-col space-y-4 text-sm">
+          <HoveredLink href="/web-dev">All courses</HoveredLink>
+          <HoveredLink href="/web-dev">Basic Music Theory</HoveredLink>
+          <HoveredLink href="/web-dev">Advanced Composition</HoveredLink>
+          <HoveredLink href="/web-dev">Songwriting</HoveredLink>
+          <HoveredLink href="/web-dev">Music Production</HoveredLink>
+        </div>
+      </MenuItem>
+
+      <MenuItem setActive={setActive} active={active} item="Contact Us">
+      </MenuItem>
+    </Menu>
   </div>
 }
