f161c51ba85a28f8ea79cc9f149e9ecbeb8487dc
vinsmokejazz <vinsmokejazz@gmail.com>
2025-05-13 22:01:34 +0530

    upd

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -33,7 +33,7 @@ export default function HeroSection() {
         borderRadius="1.75rem"
         className="bg-white dark:bg-slate-950 text-black dark:text-white border-neutral-200 dark:border-slate-800"
       >
-        Borders are cool
+        Explore courses
       </Button>
         </Link>
       </div>
