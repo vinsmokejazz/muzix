ba3969bd79c1318a18702019c5f163b641c2ee11
vinsmokejazz <vinsmokejazz@gmail.com>
2025-05-13 21:52:45 +0530

    added herosection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import HeroSection from "@/components/HeroSection";
+
 
 export default function Home() {
   return (
-    <div className="flex justify-center text-2xl min-h-screen items-center">
-      empty
-    </div>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02">
+      
+      <HeroSection/>
+    </main>
   );
 }
