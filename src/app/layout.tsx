74c0ec640f387dbce90615f39daef42ce8bfa80b
vinsmokejazz <vinsmokejazz@gmail.com>
2025-05-13 14:50:33 +0530

    sm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="relative w-full flex items-center justify-center">
         <Navbar/>
+        </div>
         {children}
       </body>
     </html>
