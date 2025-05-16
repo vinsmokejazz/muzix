19a060e716d6e135b9d430f981bf261c6b79d097
vinsmokejazz <vinsmokejazz@gmail.com>
2025-05-16 23:49:38 +0530

    instructor section

---
+++ src/components/Instructors.tsx
@@ -0,0 +1,12 @@
+import { WavyBackground } from "./ui/wavy-background";
+
+export default function Instructors(){
+
+  return <div className="  relative h-[40rem] flex items-center overflow-hidden justify-center">
+    <WavyBackground className="w-full max-w-7xl mx-auto flex flex-col items-center  justify-center h-full">
+      <h2 className="text-2xl md:text-4xl lg:text-7xl text-white font-bold text-center mb-8">
+        Meet Our Instructors
+      </h2>
+    </WavyBackground>
+  </div>
+}
