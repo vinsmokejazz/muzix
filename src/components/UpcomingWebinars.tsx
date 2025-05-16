5d297a73d1ac583dffb212042743759c76eb0155
vinsmokejazz <vinsmokejazz@gmail.com>
2025-05-16 23:24:26 +0530

    chng

---
+++ src/components/UpcomingWebinars.tsx
@@ -0,0 +1,14 @@
+export default function UpcominWebinars(){
+
+  return (
+    <div className="p-12 bg-gray-900">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6">
+        <div className="text-base text-teal-600 font-semibold tracking-wide uppercase">FEATURED WEBINARS</div>
+<h2 className="text-base text-teal-600 font-semibold tracking-wide uppercase">FEATURED WEBINARS</h2>
+          <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">Enhance Your Musical Journey</p>
+
+      </div>
+
+    </div>
+  )
+}
