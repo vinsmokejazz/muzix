f870a614ccd89519e50b82afbbf9cb6f21a3fb75
vinsmokejazz <vinsmokejazz@gmail.com>
2025-05-16 23:32:28 +0530

    updated card hover

--- src/components/UpcomingWebinars.tsx
+++ src/components/UpcomingWebinars.tsx
@@ -1,4 +1,8 @@
+import Link from "next/link";
+import { HoverEffect } from "./ui/HoverEffect";
+
 export default function UpcominWebinars() {
+
   const featuredWebinars = [
     {
       title: 'Understanding Music Theory',
@@ -44,7 +48,7 @@ export default function UpcominWebinars() {
       isFeatured: true,
     },
   ];
-  return (
+   return (
     <div className="p-12 bg-gray-900">
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
         <div className="text-center">
@@ -54,14 +58,22 @@ export default function UpcominWebinars() {
 
         <div className="mt-10">
           <HoverEffect
-            items={featuredWebinars.map(webinar => (
-              {
-                title: webinar.title,
-                description: webinar.description,
-                link: '/'
-              }
-            ))}
+          items={featuredWebinars.map(webinar => (
+            {
+              title: webinar.title,
+              description: webinar.description,
+              link: '/'
+            }
+          ))}
           />
+        </div>
+
+        <div className="mt-10 text-center">
+          <Link href={"/"}
+          className="px-4 py-2 rounded border border-neutral-600 text-neutral-700 bg-white hover:bg-gray-100 transition duration-200"
+          >
+            View All webinars
+          </Link>
         </div>
       </div>
     </div>
